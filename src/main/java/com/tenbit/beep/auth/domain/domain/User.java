c5ffdffc9c462d8b04d8077ed242cefb37f0c929
황정빈 <hjbin1211@gmail.com>
2025-07-30 20:39:04 +0900

    feat: User entity class addition

---
+++ src/main/java/com/tenbit/beep/auth/domain/domain/User.java
@@ -0,0 +1,43 @@
+package com.tenbit.beep.auth.domain.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long innerId;
+
+    @Column(nullable = false)
+    @Min(0)
+    @Max(4000)
+    // 선생님은 0000
+    private int studentNumber;
+
+    @Column(unique = true, nullable = false, length = 10)
+    @Size(min = 2, max = 10)
+    private String name;
+
+    @Column(unique = true, nullable = false, length = 15)
+    @Size(min = 4, max = 15)
+    private String publicId;
+
+    @Column(nullable = false, length = 255)
+    @Size(min = 4, max = 255)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    @Email
+    private String email;
+}
