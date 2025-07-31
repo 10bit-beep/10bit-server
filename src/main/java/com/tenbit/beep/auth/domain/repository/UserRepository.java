8c101d622528f0eca8a2117e1661410fb34c8089
황정빈 <hjbin1211@gmail.com>
2025-08-01 00:52:30 +0900

    feat: Repository annotation addition

--- src/main/java/com/tenbit/beep/auth/domain/repository/UserRepository.java
+++ src/main/java/com/tenbit/beep/auth/domain/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.tenbit.beep.auth.domain.repository;
 
 import com.tenbit.beep.auth.domain.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByPublicId(String publicId);
