06d3a79084afc86262d9eee92e1f7a45166cbb4b
이도연 <dgsw0509@dgsw.hs.kr>
2025-08-10 14:31:52 +0900

    fix: typo

--- src/main/java/com/tenbit/beep/attendance/service/AttendanceService.java
+++ src/main/java/com/tenbit/beep/attendance/service/AttendanceService.java
@@ -2,5 +2,5 @@ package com.tenbit.beep.attendance.service;
 
 public interface AttendanceService {
     void markAttendance(String publicId);
-    void resetALlAttendance();
+    void resetAllAttendance();
 }
