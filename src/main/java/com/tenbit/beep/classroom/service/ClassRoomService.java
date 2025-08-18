149097f2965685c06efc08d2eedad7c79176e740
황정빈 <hjbin1211@gmail.com>
2025-08-18 18:16:47 +0900

    feat: classroom part dto addition

--- src/main/java/com/tenbit/beep/classroom/service/ClassRoomService.java
+++ src/main/java/com/tenbit/beep/classroom/service/ClassRoomService.java
@@ -2,11 +2,12 @@ package com.tenbit.beep.classroom.service;
 
 import com.tenbit.beep.auth.domain.User;
 import com.tenbit.beep.classroom.domain.ClassRoom;
+import com.tenbit.beep.classroom.dto.LookUpStudentsRequest;
 
 import java.util.List;
 
 public interface ClassRoomService {
 
-    List<ClassRoom> lookUpStudentsByClassRoom(String classRoomName);
-    List<User> lookUpStudentsByPrimaryClassRoom(String primaryClassRoomName);
+    List<ClassRoom> lookUpStudentsByClassRoom(LookUpStudentsRequest lookUpStudentsRequest);
+    List<User> lookUpStudentsByPrimaryClassRoom(LookUpStudentsRequest lookUpStudentsRequest);
 }
--- src/main/java/com/tenbit/beep/classroom/service/impl/ClassRoomServiceImpl.java
+++ src/main/java/com/tenbit/beep/classroom/service/impl/ClassRoomServiceImpl.java
@@ -2,6 +2,7 @@ package com.tenbit.beep.classroom.service.impl;
 
 import com.tenbit.beep.auth.domain.User;
 import com.tenbit.beep.classroom.domain.ClassRoom;
+import com.tenbit.beep.classroom.dto.LookUpStudentsRequest;
 import com.tenbit.beep.classroom.repository.ClassRoomRespository;
 import com.tenbit.beep.classroom.service.ClassRoomService;
 import lombok.RequiredArgsConstructor;
@@ -17,21 +18,21 @@ public class ClassRoomServiceImpl implements ClassRoomService {
     private final ClassRoomRespository classRoomRespository;
 
     @Override
-    public List<ClassRoom> lookUpStudentsByClassRoom(String classRoomName) {
+    public List<ClassRoom> lookUpStudentsByClassRoom(LookUpStudentsRequest lookUpStudentsRequest) {
 
-        List<ClassRoom> students = classRoomRespository.findByClassRoomName(classRoomName);
+        List<ClassRoom> students = classRoomRespository.findByClassRoomName(lookUpStudentsRequest.getClassRoomName());
 
         if (students.isEmpty()) {
-            lookUpStudentsByPrimaryClassRoom(classRoomName);
+            lookUpStudentsByPrimaryClassRoom(lookUpStudentsRequest);
         }
 
         return students;
     }
 
     @Override
-    public List<User> lookUpStudentsByPrimaryClassRoom(String primaryClassRoomName) {
+    public List<User> lookUpStudentsByPrimaryClassRoom(LookUpStudentsRequest lookUpStudentsRequest) {
 
-        List<User> students = classRoomRespository.findByPrimaryClassRoomName(primaryClassRoomName);
+        List<User> students = classRoomRespository.findByPrimaryClassRoomName(lookUpStudentsRequest.getClassRoomName());
 
         return students;
     }
