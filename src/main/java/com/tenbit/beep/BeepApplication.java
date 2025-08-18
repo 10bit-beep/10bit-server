edb9da448b360a4a387a71cc33af84fc1b248a26
황정빈 <hjbin1211@gmail.com>
2025-08-18 15:29:32 +0900

    feat: DI addition

--- src/main/java/com/tenbit/beep/BeepApplication.java
+++ src/main/java/com/tenbit/beep/BeepApplication.java
@@ -1,7 +1,11 @@
 package com.tenbit.beep;
 
+import com.tenbit.beep.auth.repository.UserRepository;
+import com.tenbit.beep.auth.service.DBService;
+import com.tenbit.beep.auth.service.impl.DBServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BeepApplication {
@@ -9,12 +13,12 @@ public class BeepApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeepApplication.class, args);
 
-//		ApplicationContext context = SpringApplication.run(BeepApplication.class, args);
-//		UserRepository userRepository = context.getBean(UserRepository.class);
-//		DBService dbService = new DBServiceImpl(userRepository);
-//		dbService.resetDB();
+		ApplicationContext context = SpringApplication.run(BeepApplication.class, args);
+		UserRepository userRepository = context.getBean(UserRepository.class);
+		DBService dbService = new DBServiceImpl(userRepository);
+		dbService.resetDB();
 
-//		dbService.deleteByPublicId("");
+		dbService.deleteByPublicId("");
 	}
 
 }
