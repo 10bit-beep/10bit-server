e234b72058f57b6d4aa34a6c3d7f00155bd8df43
이도연 <dgsw0509@dgsw.hs.kr>
2025-08-19 22:09:25 +0900

    feat: auto checkout

--- src/main/java/com/tenbit/beep/attendance/service/impl/AttendanceServiceImpl.java
+++ src/main/java/com/tenbit/beep/attendance/service/impl/AttendanceServiceImpl.java
@@ -6,6 +6,7 @@ import com.tenbit.beep.attendance.domain.Attendance;
 import com.tenbit.beep.auth.domain.User;
 import com.tenbit.beep.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,13 +17,23 @@ import java.util.List;
 public class AttendanceServiceImpl implements AttendanceService {
 
     private final UserRepository userRepository;
+    private final ThreadPoolTaskScheduler taskScheduler;
+
 
     @Override
     public void markAttendance(String publicId) {
         User user = userRepository.findByPublicId(publicId)
-                .orElseThrow(() -> new IllegalArgumentException("사용자 찾을 수 없음."));
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없음."));
         user.setAttendance(Attendance.TRUE);
         userRepository.save(user);
+
+        taskScheduler.schedule(
+                () -> {
+                    user.setAttendance(Attendance.FALSE);
+                    userRepository.save(user);
+                },
+                java.util.Date.from(java.time.Instant.now().plusSeconds(30))
+        );
     }
 
     @Override
