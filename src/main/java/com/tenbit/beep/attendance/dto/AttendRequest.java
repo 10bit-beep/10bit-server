e230b3c75b465a3ce0ae6b5a3fbd70f3fef77c3d
이도연 <dgsw0509@dgsw.hs.kr>
2025-09-24 08:33:37 +0900

    fix : add NonNull

--- src/main/java/com/tenbit/beep/attendance/dto/AttendRequest.java
+++ src/main/java/com/tenbit/beep/attendance/dto/AttendRequest.java
@@ -1,12 +1,13 @@
 package com.tenbit.beep.attendance.dto;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AttendRequest {
-
+    @NonNull
     private String publicId;
     private String nfcTag;
 }
